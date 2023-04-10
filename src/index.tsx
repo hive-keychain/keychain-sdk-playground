e5838810f55abdef581d2d25c2ec45dfaf5bce09
stoodkev <quentin.correa@gmail.com>
2023-04-10 15:07:49 +0800

    make hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import ErrorPage from "./components/pages/error-page";
 import RequestCard from "./components/routes/request-card";
 import RootLayout from "./components/routes/root-layout";
 import "./index.css";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <RootLayout />,
